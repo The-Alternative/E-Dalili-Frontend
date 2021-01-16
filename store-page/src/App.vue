b2dd688345a80a33c9fa1bbc145aea043c822f01
rahaf.rj.jamool <rahaf.rj.jamool@gmail.com>
2021-01-16 08:46:08 +0200

    first project

---
+++ store-page/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <BodyStore />
+    </div>
+</template>
+
+<script>
+import BodyStore from './components/BodyStore.vue';
+
+export default {
+    name: 'app',
+    components: {
+        BodyStore,
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+</style>
