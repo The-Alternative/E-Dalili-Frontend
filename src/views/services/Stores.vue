a9e2b2bdd58bc7ff4ca555b503940ae2d826b32e
rahaf.rj.jamool <rahaf.rj.jamool@gmail.com>
2021-02-10 13:33:05 +0200

    add file

---
+++ src/views/services/Stores.vue
@@ -0,0 +1,15 @@
+<template>
+    <BodyStore />
+</template>
+
+<script>
+import BodyStore from '../../components/body/BodyStore';
+export default {
+    name: 'Stores',
+    components: {
+        BodyStore,
+    },
+};
+</script>
+
+<style scoped></style>
