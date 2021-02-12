761c547b96609f81ad323224b676997eaac4e836
rahaf.rj.jamool <rahaf.rj.jamool@gmail.com>
2021-02-12 21:52:37 +0200

    all responsive

--- src/main.js
+++ src/main.js
@@ -1,41 +1,37 @@
-import Vue from "vue";
+import Vue from 'vue';
 import VueGeolocation from 'vue-browser-geolocation';
 import * as VueGoogleMaps from 'vue2-google-maps';
 import SuiVue from 'semantic-ui-vue';
-import App from "./App.vue";
-import VueRouter from 'vue-router'
-import router from "./router/index";
-import store from "./store/Store";
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import router from './router/index';
+import store from './store/Store';
 import './js/body';
-import "bootstrap";
-import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import "normalize.css";
-import "../public/fontawesome-free-5.15.1-web/css/all.css";
+import 'bootstrap';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'normalize.css';
+import main from '../src/assets/css/main.css';
+import '../public/fontawesome-free-5.15.1-web/css/all.css';
 
 Vue.config.productionTip = false;
 Vue.use(VueGeolocation);
 Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyDjjb4f0kJ19pYIRiosKb-MOofau97t80A',
-  },
-  installComponents: false,
+    load: {
+        key: 'AIzaSyDjjb4f0kJ19pYIRiosKb-MOofau97t80A',
+    },
+    installComponents: false,
 });
 
 Vue.use(SuiVue);
-Vue.use(VueRouter)
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App)
-}).$mount("#app");
-
-
-
-
-
-
+    router,
+    store,
+    main,
+    render: (h) => h(App),
+}).$mount('#app');
