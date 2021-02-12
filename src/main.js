a5dcfdceeb4a4e35d753b3c54dea4a9d24efa0e5
rahaf.rj.jamool <rahaf.rj.jamool@gmail.com>
2021-02-12 16:29:58 +0200

    all responsive

--- src/main.js
+++ src/main.js
@@ -6,13 +6,12 @@ import App from "./App.vue";
 import VueRouter from 'vue-router'
 import router from "./router/index";
 import store from "./store/Store";
-
+import './js/body';
 import "bootstrap";
 import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "normalize.css";
-import main from "../src/assets/css/main.css";
 import "../public/fontawesome-free-5.15.1-web/css/all.css";
 
 Vue.config.productionTip = false;
