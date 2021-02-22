ff37c50e0b4b1782fd1f73fee594c31a4a3ef7a7
rahaf-jamool <rahafjamool1@gmail.com>
2021-02-22 21:58:45 +0200

    add css hover

--- src/components/body/location-store.vue
+++ src/components/body/location-store.vue
@@ -20,7 +20,13 @@ export default {
 @media (min-width: 200px) and (max-width: 467px) {
     .location {
         height: 15px;
-        margin-left: 60px;
+        margin-left: 40px;
+        border-radius: 50px;
+        width: 15px;
+        transition: all 0.8s;
+    }
+    .location:hover {
+        transform: translate(0, -10px);
     }
 }
 @media (min-width: 468px) and (max-width: 598px) {
@@ -28,31 +34,64 @@ export default {
         height: 15px;
         margin-left: 130px;
         margin-right: 10px;
+        margin-right: 10px;
+        border-radius: 50px;
+        width: 15px;
+        transition: all 0.8s;
+    }
+    .location:hover {
+        transform: translate(0, -10px);
     }
 }
 @media (min-width: 599px) and (max-width: 767px) {
     .location {
         height: 15px;
         margin-left: 90px;
-        margin-right: 10px;
+        border-radius: 50px;
+        width: 15px;
+        transition: all 0.8s;
+    }
+    .location:hover {
+        transform: translate(0, -10px);
     }
 }
 @media (min-width: 768px) and (max-width: 991.98px) {
     .location {
         margin-left: 80px;
         margin-right: 10px;
+        margin-right: 10px;
+        border-radius: 50px;
+        width: 15px;
+        transition: all 0.8s;
+    }
+    .location:hover {
+        transform: translate(0, -10px);
     }
 }
 @media (min-width: 992px) and (max-width: 1199.98px) {
     .location {
         margin-left: 120px;
         margin-right: 10px;
+        margin-right: 10px;
+        border-radius: 50px;
+        width: 15px;
+        transition: all 0.8s;
+    }
+    .location:hover {
+        transform: translate(0, -10px);
     }
 }
 @media (min-width: 1200px) {
     .location {
         margin-left: 190px;
         margin-right: 10px;
+        margin-right: 10px;
+        border-radius: 50px;
+        width: 15px;
+        transition: all 0.8s;
+    }
+    .location:hover {
+        transform: translate(0, -10px);
     }
 }
 * {
--- src/components/global/BodyProduct.vue
+++ src/components/global/BodyProduct.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6 coler">
         <div class="content-pro text-center">
-            <img @click="goto(id, title, description, price)"
-             src="@/assets/w.jpg" class="new" />
-            <div @click="goto(id, title, description, price)" class="name-prod">{{ title }}</div>
-            <div @click="goto(id, title, description, price)" class="category">{{ description }}</div>
+            <img
+                @click="goto(id, title, description, price)"
+                src="@/assets/w.jpg"
+                class="new"
+            />
+            <div @click="goto(id, title, description, price)" class="name-prod">
+                {{ title }}
+            </div>
+            <div @click="goto(id, title, description, price)" class="category">
+                {{ description }}
+            </div>
 
             <div class="stars">
                 <span class="fa fa-star  checked"></span>
@@ -22,118 +29,118 @@
                 <span class="fa fa-check-circle"></span>
             </div>
             <div class="all">
-
-<div class="left" @click="goto(id, title, description, price)">
-  <div class="text">تفاصيل المنتج</div>
-</div>
-<div class="center">
-  <div class="explainer"><span><i class="fa fa-shopping-cart"></i
-            ></span></div>
-  <div class="text">أختيار</div>
-  </div>
-<div class="right" @click="addToCart">
-  <div class="text">أضف للعربة </div>
-</div>
+                <div class="left" @click="goto(id, title, description, price)">
+                    <div class="text">تفاصيل المنتج</div>
+                </div>
+                <div class="center">
+                    <div class="explainer">
+                        <span><i class="fa fa-shopping-cart"></i></span>
+                    </div>
+                    <div class="text">أختيار</div>
+                </div>
+                <div class="right" @click="addToCart">
+                    <div class="text">أضف للعربة</div>
+                </div>
             </div>
-            
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
 .all {
-margin: 20px 0;
-display: flex;
-perspective: 10px;
-transform: perspective(250px) rotateX(20deg);
-will-change: perspective;
-perspective-origin: center center;
-transition: all 1.3s ease-out;
-justify-content: center;
-transform-style: preserve-3d;
+    margin: 20px 0;
+    display: flex;
+    perspective: 10px;
+    transform: perspective(250px) rotateX(20deg);
+    will-change: perspective;
+    perspective-origin: center center;
+    transition: all 1.3s ease-out;
+    justify-content: center;
+    transform-style: preserve-3d;
 }
 
 .all:hover {
-  perspective: 1000px;
-  transition: all 1.3s ease-in;
-  transform: perspective(10000px) rotateX(0deg);
-  .text {
-    opacity: 1;
-  }
-  & > div {
-    opacity: 1;
-    transition-delay: 0s;
-  }
-  .explainer {
-    opacity: 0;
-  }
+    perspective: 1000px;
+    transition: all 1.3s ease-in;
+    transform: perspective(10000px) rotateX(0deg);
+    .text {
+        opacity: 1;
+    }
+    & > div {
+        opacity: 1;
+        transition-delay: 0s;
+    }
+    .explainer {
+        opacity: 0;
+    }
 }
 
-.left, .center, .right {
-  width: 50px;
-  height:60px;
-  transform-style: preserve-3d;
-  border-radius: 10px;
-  border: 1px solid #fff;
-  box-shadow: 0 0 20px 5px rgba(142, 142, 170, 0.4);
-  opacity: 0;
-  transition: all .3s ease;
-  transition-delay: 1s;
-  position: relative;
-  background-position: center center;
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-color: #949ba6;
-  background-blend-mode: color-burn;
-  cursor: pointer;
-  
-  &:hover {
-    box-shadow: 0 0 30px 10px rgba(141, 141, 179, 0.6);
-    background-color: #ccf;
-  }
+.left,
+.center,
+.right {
+    width: 50px;
+    height: 60px;
+    transform-style: preserve-3d;
+    border-radius: 10px;
+    border: 1px solid #fff;
+    box-shadow: 0 0 20px 5px rgba(142, 142, 170, 0.4);
+    opacity: 0;
+    transition: all 0.3s ease;
+    transition-delay: 1s;
+    position: relative;
+    background-position: center center;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-color: #949ba6;
+    background-blend-mode: color-burn;
+    cursor: pointer;
+
+    &:hover {
+        box-shadow: 0 0 30px 10px rgba(141, 141, 179, 0.6);
+        background-color: #ccf;
+    }
 }
 .text {
-  transform: translateY(30px);
-  opacity: 0;
-  transition: all .3s ease;
+    transform: translateY(30px);
+    opacity: 0;
+    transition: all 0.3s ease;
     bottom: -4px;
     left: -23px;
-  position: absolute;
-  will-change: transform;
-  color: #2f2e2e;
-  text-shadow: 0 0 5px rgba(44, 44, 44, 0.6);
-  width: 100px;
-  font-size: 14px;
+    position: absolute;
+    will-change: transform;
+    color: #2f2e2e;
+    text-shadow: 0 0 5px rgba(44, 44, 44, 0.6);
+    width: 100px;
+    font-size: 14px;
 }
 
 .left {
-  transform: translateX(-30px) translateZ(-25px) rotateY(-5deg);
-  background-image: url(https://cdn3.iconfinder.com/data/icons/other-icons/48/app_window-512.png);
+    transform: translateX(-30px) translateZ(-25px) rotateY(-5deg);
+    background-image: url(https://cdn3.iconfinder.com/data/icons/other-icons/48/app_window-512.png);
 }
 .center {
-  opacity: 1;
- background-image: url(https://image.flaticon.com/icons/png/128/724/724802.png);
+    opacity: 1;
+    background-image: url(https://image.flaticon.com/icons/png/128/724/724802.png);
 }
 .right {
-  transform: translateX(30px) translateZ(-25px) rotateY(5deg);
-  background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhcnzP6OhuBuJHzrtSe0aWPwzbO2dxlG-d8g&usqp=CAU);
+    transform: translateX(30px) translateZ(-25px) rotateY(5deg);
+    background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhcnzP6OhuBuJHzrtSe0aWPwzbO2dxlG-d8g&usqp=CAU);
 }
 .explainer {
-  font-weight: 300;
-  font-size: 2rem;
-  color: #fff;
-  transition: all .6s ease;
-  width: 100%;
-  height: 100%;
-  background-color: #303050;
-  background-image: radial-gradient(circle at center top, #cce, #9090ac);
-  border-radius: 10px;
-  text-shadow: 0 0 10px rgba(255, 255, 255, .8);
-  
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+    font-weight: 300;
+    font-size: 2rem;
+    color: #fff;
+    transition: all 0.6s ease;
+    width: 100%;
+    height: 100%;
+    background-color: #303050;
+    background-image: radial-gradient(circle at center top, #cce, #9090ac);
+    border-radius: 10px;
+    text-shadow: 0 0 10px rgba(255, 255, 255, 0.8);
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
 <script>
 export default {
@@ -193,22 +200,24 @@ export default {
 
 <style scoped>
 @media (max-width: 575.98px) {
-  .left, .center, .right {
-  width: 40px;
-  height:45px;
-  }
-  .left {
-  transform: translateX(-10px) translateZ(-25px) rotateY(-5deg);
-}
-.right {
-  transform: translateX(10px) translateZ(-25px) rotateY(5deg);
-}
-.text{
-bottom: -7px;
-left: 5px;
-width: 0px;
-font-size: 13px;
-}
+    .left,
+    .center,
+    .right {
+        width: 40px;
+        height: 45px;
+    }
+    .left {
+        transform: translateX(-10px) translateZ(-25px) rotateY(-5deg);
+    }
+    .right {
+        transform: translateX(10px) translateZ(-25px) rotateY(5deg);
+    }
+    .text {
+        bottom: -7px;
+        left: 5px;
+        width: 0px;
+        font-size: 13px;
+    }
 }
 /* body of products */
 .cartshop {
@@ -242,7 +251,7 @@ font-size: 13px;
     cursor: pointer;
 }
 .content-pro .name-prod,
-.content-pro .category{
+.content-pro .category {
     cursor: pointer;
 }
 .content-pro .stars {
