e1bb04d67f79a37a3b8d9de3783e531f914ad7d7
alaa <alaamhna3354@gmail.com>
2021-02-25 10:19:45 -0800

    add some

--- src/components/cart/Cartmini.vue
+++ src/components/cart/Cartmini.vue
@@ -8,7 +8,7 @@
             <div class="row">
                 <div class="col-md-12 cart">
                    
-               
+               <!-- ____ -->
                    
                     <div
                         v-for="items in cartItems"
